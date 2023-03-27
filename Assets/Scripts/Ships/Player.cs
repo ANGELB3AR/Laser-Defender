b41c7be9ab77775e1b974ca09052d054122aa2d6
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-27 07:49:53 -0500

    Switched to Client-Auth movement

--- Assets/Scripts/Ships/Player.cs
+++ Assets/Scripts/Ships/Player.cs
@@ -36,7 +36,6 @@ public class Player : NetworkBehaviour
         if (!Application.isFocused) { return; }
 
         Move();
-        Debug.Log($"Player {OwnerClientId} is Owner = {IsOwner}");
     }
 
     void InitializeBounds()
@@ -48,33 +47,18 @@ public class Player : NetworkBehaviour
 
     private void Move()
     {
-        Debug.Log($"Currently moving with {rawInput} input");
         Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
         Vector2 newPos = new Vector2();
 
         newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
         newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
-        Debug.Log("New Position: " + newPos);
 
-        MoveServerRpc(newPos);
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    void MoveServerRpc(Vector2 newPos)
-    {
         transform.position = newPos;
-        DebugLogger(newPos);
-    }
-
-    void DebugLogger(Vector2 newPos)
-    {
-        Debug.Log($"Move RPC called: moving to {newPos}");
     }
 
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        Debug.Log($"Raw Input: {rawInput}");
     }
 
     void OnFire(InputValue value)
