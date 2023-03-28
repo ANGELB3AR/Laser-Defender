9e10771034803c90e42a08d4fc60751f5655396c
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-28 11:09:06 -0500

    Enemies spawn on server [BUG: Enemies are destroying themselves]

--- Assets/Scripts/EnemyAI/EnemySpawner.cs
+++ Assets/Scripts/EnemyAI/EnemySpawner.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : NetworkBehaviour
 {
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
@@ -10,8 +11,10 @@ public class EnemySpawner : MonoBehaviour
     
     WaveConfigSO currentWave;
 
-    void Start()
+    public override void OnNetworkSpawn()
     {
+        if (!IsServer) { return; }
+
         StartCoroutine(SpawnEnemyWaves());
     }
 
@@ -24,10 +27,8 @@ public class EnemySpawner : MonoBehaviour
                 currentWave = wave;
                 for (int i = 0; i < currentWave.GetEnemyCount(); i++)
                 {
-                    Instantiate(currentWave.GetEnemyPrefab(i),
-                        currentWave.GetStartingWaypoint().position,
-                        Quaternion.Euler(0,0,180),
-                        transform);
+                    SpawnEnemyServerRpc(i);
+                    
                     yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
                 }
                 yield return new WaitForSeconds(timeBetweenWaves);
@@ -39,5 +40,15 @@ public class EnemySpawner : MonoBehaviour
     public WaveConfigSO GetCurrentWave()
     {
         return currentWave;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void SpawnEnemyServerRpc(int enemyPrefabIndex)
+    {
+        GameObject enemy = Instantiate(currentWave.GetEnemyPrefab(enemyPrefabIndex),
+                        currentWave.GetStartingWaypoint().position,
+                        Quaternion.Euler(0, 0, 180),
+                        transform);
+        enemy.GetComponent<NetworkObject>().Spawn();
     }
 }
